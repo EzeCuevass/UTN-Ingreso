fc70cd5befb14e9583af6eceed3fc129175c666d
gh0s7face <eze.cuevass@gmail.com>
2024-07-02 20:23:40 -0300

    Arreglo act 1

--- Unidad 1/Actividades1A.py
+++ Unidad 1/Actividades1A.py
@@ -1,15 +1,26 @@
+# 1) 
 # print("Hola UTN")
+# 2) 
 # print(3+5)
+# 3)
 # print(10-5)
+# 4)
 # print(10-15)
+# 5)
 # print(4*10)
+# 6)
 # print(10/2)
-### print(10/0)
+# 7)
+#print(10/0)
 #Cuando se divide por 0 arroja un error, debido a que la divison por 0 es imposible, el error es el siguiente: 
 #ZeroDivisionError: division by zero
+# 8)
 # print((10+3)*(6+6))
-# print(10+3*6+6)
+# 9)
 # print(10%2)
+# 10)
+# print(10%3)
+# 11)
 # variableA = 10
 # variableB = 5
 # print(variableA+variableB)
