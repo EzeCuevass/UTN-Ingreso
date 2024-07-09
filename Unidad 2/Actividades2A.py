57f2eb820497255a4111a1c6546c0859345f731f
gh0s7face <eze.cuevass@gmail.com>
2024-07-09 20:04:29 -0300

    Actividades unidad 2

---
+++ Unidad 2/Actividades2A.py
@@ -0,0 +1,32 @@
+# 1)
+# edad = int(input("Introduce tu edad: "))
+# if edad==18:
+#     print("Usted tiene 18 años")
+# 2)
+# edad = int(input("Introduce tu edad: "))
+# if edad >= 18:
+#     print("MAYOR")
+# else:
+#     print("MENOR")
+# 3)
+# altura = float(input("Introduce tu altura: "))
+# if altura >= 1.80:
+#     print("Usted puede ser pivot de basquetbol")
+# else:
+#     print("Usted NO puede ser pivot de basquetbol")
+#4)
+# edad = int(input("Introduce tu edad: "))
+# if edad >= 13 and edad <=17:
+#     print("Usted es adolescente")
+# else:
+#     print("Usted NO es adolescente")
+#5)
+# edad = int(input("Introduce tu edad: "))
+# if edad <=10:
+#     print("Usted es un niño")
+# elif edad >= 10 and edad <13:
+#     print("Usted es pre-adolescente")
+# elif edad >= 13 and edad <=17:
+#     print("Usted es adolescente")
+# else: 
+#     print("Usted es mayor de edad")
---
+++ Unidad 2/Actividades2B.py
@@ -0,0 +1,40 @@
+# 1)
+# No entendi
+#2)
+# contraseña = input("Ingrese su contraseña: ")
+# while contraseña != "utn750":
+#     if contraseña == "utn750":
+#         print("Ingreso exitoso!")
+#     else: 
+#         contraseña = input("Intente otra vez: ")
+# 3)
+# numero = int(input("Ingrese un numero entre el 0 y el 9: "))
+# while not (numero >=0 and numero <=9):
+#     numero = int(input("Intente otra vez: "))
+# 4)
+# letra = input("Ingrese una letra: ")
+
+# while letra != "U" or letra != "T" or letra != "N":
+#     match letra:
+#         case "U":
+#             print("Bien hecho")
+#             break
+#         case "T":
+#             print("Bien hecho")
+#             break
+#         case "N":
+#             print("Bien hecho")
+#             break
+#         case _:
+#             letra = input("Intente otra vez: ")
+# 5)
+# numero1 = int(input("Ingrese un numero: "))
+# numero2 = int(input("Ingrese un numero: "))
+# numero3 = int(input("Ingrese un numero: "))
+# numero4 = int(input("Ingrese un numero: "))
+# numero5 = int(input("Ingrese un numero: "))
+
+# suma = numero1 + numero2 + numero3 + numero4 + numero5
+# promedio = suma /5
+
+# print("La suma de estos numero da un total de", suma, "y el promedio es de", promedio)
